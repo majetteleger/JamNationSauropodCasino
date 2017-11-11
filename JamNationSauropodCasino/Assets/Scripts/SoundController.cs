e3ae106ee978490762bf2a85ced425c7d48d1477
Frederic <frederic@sauropodstudio.com>
2017-11-11 13:29:55 -0500

    Remodeled rocket prefab to support sound

---
+++ JamNationSauropodCasino/Assets/Scripts/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class SoundController : MonoBehaviour
+    {
+        public AudioSource fuse;
+        public AudioSource thruster;
+        public AudioSource detonation;
+        public AudioSource postDetonation;
+
+        public void TriggerSound()
+        {
+
+        }
+    }
+}
