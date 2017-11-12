a30ca6cc626a9b46d91c4307759fc8ffe104babe
Frederic <frederic@sauropodstudio.com>
2017-11-11 20:55:13 -0500

    Button mapping

---
+++ JamNationSauropodCasino/Assets/Scripts/InputTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class InputTest : MonoBehaviour
+    {
+        void Update()
+        {
+            if (ButtonManager.GetButtonDown(Button.A, Player.Any))
+                Debug.Log("A down");
+            if (ButtonManager.GetButtonUp(Button.A, Player.Any))
+                Debug.Log("A up");
+            if (ButtonManager.GetButtonDown(Button.B, Player.P1))
+                Debug.Log("B");
+            if (ButtonManager.GetButtonDown(Button.R2, Player.Any))
+                Debug.Log("R2 down");
+            //if (ButtonManager.GetButtonUp(Button.R2, Player.Any))
+            //    Debug.Log("R2 up");
+
+            if (ButtonManager.GetButtonDown(Button.A, Player.Any))
+            {
+                float debugZ = Input.GetAxis("TriggerZAxis_All");
+                Debug.Log("Z : " + debugZ);
+                float debugX = Input.GetAxis("DPadAxisX_All");
+                Debug.Log("X : " + debugX);
+                float debugY = Input.GetAxis("DPadAxisY_All");
+                Debug.Log("Y : " + debugY);
+            }
+        }
+    }
+}
