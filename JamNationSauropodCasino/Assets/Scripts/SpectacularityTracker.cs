076d4932f0f3307aef8f6e06a6cb8da6d85af927
majetteleger <ma_jetteleger@hotmail.com>
2017-11-11 22:20:03 -0500

    Crowd

--- JamNationSauropodCasino/Assets/Scripts/SpectacularityTracker.cs
+++ JamNationSauropodCasino/Assets/Scripts/SpectacularityTracker.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
@@ -30,6 +29,14 @@ namespace Assets.Scripts
         public float GrandFinaleBonus = 0.4f;
         public float TimedBlastBonus = 0.2f;
 
+        public float UpdateCrowdHandsTime;
+        private float _updateCrowdHandsTimer;
+        public float UpdateCrowdFlipTime;
+        private float _updateCrowdFlipTimer;
+        public float CrowdExcitementBuffer;
+
+        public Folk[] Crowd { get; set; }
+
         private float _lastLossTick;
         private Dictionary<Rocket, float> _recentDetonations = new Dictionary<Rocket, float>();
         private List<Rocket> _usedInCombo = new List<Rocket>();
@@ -42,12 +49,67 @@ namespace Assets.Scripts
         private void Start()
         {
             SpectacularityPanel.Instance.UpdateSpectacularityGauge(CrowdExcitment, 1f);
+            Crowd = FindObjectsOfType<Folk>();
+            UpdateCrowdHands(CrowdExcitment);
+            UpdateCrowdFlip(CrowdExcitment);
+
+            _updateCrowdHandsTimer = UpdateCrowdHandsTime;
+            _updateCrowdFlipTimer = UpdateCrowdFlipTime;
         }
 
         private List<Rocket> _toRemove = new List<Rocket>();
 
+        private void UpdateCrowdHands(float excitement)
+        {
+            for(int i = 0; i < Crowd.Length; i++)
+            {
+                float rndEx = Random.Range(0.0f, 1.0f);
+
+                if (rndEx <= excitement - CrowdExcitementBuffer)
+                {
+                    Crowd[i].HandsUp();
+                }
+                else
+                {
+                    Crowd[i].HandsDown();
+                }
+            }
+        }
+
+        private void UpdateCrowdFlip(float excitement)
+        {
+            for (int i = 0; i < Crowd.Length; i++)
+            {
+                float rndFl = Random.Range(0.0f, 1.0f);
+
+                if (rndFl <= excitement - CrowdExcitementBuffer)
+                {
+                    Crowd[i].transform.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
+                }
+                else
+                {
+                    Crowd[i].transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                }
+            }
+        }
+
         private void Update()
         {
+            _updateCrowdHandsTimer -= Time.deltaTime;
+            _updateCrowdFlipTimer -= Time.deltaTime;
+
+            if(_updateCrowdHandsTimer <= 0)
+            {
+                _updateCrowdHandsTimer = UpdateCrowdHandsTime;
+                UpdateCrowdHands(CrowdExcitment);
+            }
+
+            if (_updateCrowdFlipTimer <= 0)
+            {
+                _updateCrowdFlipTimer = UpdateCrowdFlipTime;
+                UpdateCrowdFlip(CrowdExcitment);
+            }
+            
             if (Time.time - 1 > _lastLossTick)
             {
                 _lastLossTick = Time.time;
